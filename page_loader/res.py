c0e8951a407cc3b88867277569fb83a15ef8eaf6
ilnur.nasibullin <nasosurulya@gmail.com>
2022-03-13 18:30:58 +0500

    step 3

--- page_loader/res.py
+++ page_loader/res.py
@@ -1,9 +1,11 @@
 from urllib.parse import urlparse
 
+RES_TYPES = ('img', 'link', 'script')
+RES_REFS = {'src', 'href'}
 
 def is_image(resource):
     for attribute in resource.attrs:
-        if attribute == 'src':
+        if attribute in RES_REFS:
             p_url = urlparse(resource.get(attribute))
             if len(p_url.path) > 1 and not (p_url.netloc or p_url.scheme):
                 return True
@@ -11,14 +13,16 @@ def is_image(resource):
 
 
 def get_images(html_dom):
-    return list(filter(is_image, html_dom.find_all('img')))
+    return list(filter(is_image, html_dom.find_all(RES_TYPES)))
 
 
 def get_res_path(res):
-    if res.has_attr('src'):
-        return res.attrs['src']
+    for ref in RES_REFS:
+        if res.has_attr(ref):
+            return res.attrs[ref]
 
 
 def update_link(res, new_link):
-    if res.has_attr('src'):
-        res.attrs['src'] = new_link
+    for ref in RES_REFS:
+        if res.has_attr(ref):
+            res.attrs[ref] = new_link
